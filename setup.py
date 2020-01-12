81244c628ef571296028df67df9db9a856c7a76e
Rehan Guha <rehanguha29@gmail.com>
2020-01-12 22:41:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="alert", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Rehan Guha",
     py_modules=["alert"],
     license='gpl-3.0',
